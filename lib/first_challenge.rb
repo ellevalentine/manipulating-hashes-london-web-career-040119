182023df1cfac28a8f2ae912209a66f77621cd7e
Elle Valentine <elle.valentine@hotmail.com>
2019-03-29 13:06:29 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,8 +18,8 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     value.deleteif do |flavor|
-      
-    
+      flavor= "strawberry"
+    end
   end
 end
 
